1622ea10d59511036497add5dcb43915a3e659cd
qkdwodns <162937195+qkdwodns@users.noreply.github.com>
2024-05-20 21:55:24 +0900

    Add files via upload

---
+++ if_test (1).py
@@ -0,0 +1,71 @@
+'''
+#5
+if 6>3:
+    print("크다")
+print("끝")
+#7
+if 4>3:
+    print("A")
+print("B")
+#6
+if 5==5:
+    print("A")
+    print("B")
+#8
+print("비교")
+if 6==3:
+    print("틀리다")
+
+#9
+if 10>4:
+    print("big")
+else:
+    print("small")
+
+#11
+if 2+3>4:
+    print("plus")
+else:
+    print("no")
+#10
+if 10 != 10:
+    print("diff")
+else:
+    print("smae")
+
+#12
+if 5<4-1:
+    print("minus")
+else:
+    print("no")
+#
+
+key = 9
+if key <=0: 
+    print("0이하") 
+elif key <=5:
+    print("5이하") 
+else:
+    print("10이하")
+    '''
+import datetime
+now = datetime.datetime.now()
+now.month = 5
+
+if 3<= now.month <=5:
+     print("봄입니다")
+elif 6<= now.month <=8:
+    print("여름입니다")
+elif 9<= now.month <=11:
+    print("가을입니다")
+elif 1<= now.month<=2 or now.month ==12:              
+    print("겨울입니다")
+
+
+ 
+
+
+
+
+
+  
